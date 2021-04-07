1f5227c6a1b0fc96583810c851d2bef61827e4c5
LeoSL <leosilvalima@gmail.com>
2021-04-06 22:01:21 -0400

    Add initial logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import moment from "moment";
 import './App.css';
 
+const FIRST_WEEK = 15;
+
 function App() {
+  const frontendQueue = ["Dima", "Leo", "Andrii"];
+  const frontendIndexes = Object.keys(frontendQueue);
+
+  const backendQueue = ["Marco", "Leoni", "Sukh", "Daniel"];
+  const backendIndexes = Object.keys(backendQueue);
+
+  const thisWeek = moment().week();
+
+  let frontendCurrentWeek = thisWeek;
+  let backendCurrentWeek = thisWeek;
+
+  while (!frontendIndexes.includes(String(frontendCurrentWeek - FIRST_WEEK))) {
+    frontendCurrentWeek = frontendCurrentWeek - 6;
+  }
+
+  while (!backendIndexes.includes(String(backendCurrentWeek - FIRST_WEEK))) {
+    backendCurrentWeek = backendCurrentWeek - 6;
+  }
+
+  const frontendPerson = frontendQueue[frontendCurrentWeek - FIRST_WEEK];
+  const backendPerson = backendQueue[backendCurrentWeek - FIRST_WEEK];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Week: ({thisWeek}th)</h1>
+      <hr className="Divider"></hr>
+      <h2>Frontend</h2>
+      <div className="Background">
+      <p>This week's person is:</p>
+        {frontendPerson}
+      </div>
+      <h2>Backend</h2>
+      <div className="Background">
+      <p>This week's person is:</p>
+        {backendPerson}
+      </div>
     </div>
   );
 }
